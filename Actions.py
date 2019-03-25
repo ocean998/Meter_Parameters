01808c55dcf9cd586289ddf0ac2eb4c1feb73354
ocean998 <40862700+ocean998@users.noreply.github.com>
2019-03-25 14:06:34 +0800

    pyQt5 UI 后台更新

--- Actions.py
+++ Actions.py
@@ -2,69 +2,100 @@
 
 # Form implementation generated from reading ui file 'Actions.ui'
 #
-# Created by: PyQt5 UI code generator 5.11.3
+# Created by: PyQt5 UI code generator 5.12.1
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+# 导入模块
 from wxpy import *
+# from bot import register
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+class Ui_MainWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self)
 
-
-class Ui_Form(QtWidgets.QWidget):
-    def __init__(self):
-        super(Ui_Form,self).__init__()
-
-
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(827, 508)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(60, 50, 75, 23))
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(805, 604)
+        MainWindow.setStatusTip("")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(60, 90, 90, 23))
         self.pushButton.setObjectName("pushButton")
+
         self.pushButton.clicked.connect(self.open_bot)
 
-
-        self.pushButton_2 = QtWidgets.QPushButton(Form)
-        self.pushButton_2.setGeometry(QtCore.QRect(260, 50, 75, 23))
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(170, 90, 90, 23))
+        self.pushButton_2.setDefault(True)
         self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_2.clicked.connect( self.msg )
 
+        self.pushButton_2.clicked.connect(self.msg)
 
-        self.lineEdit = QtWidgets.QLineEdit(Form)
-        self.lineEdit.setGeometry(QtCore.QRect(60, 110, 113, 20))
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(700, 210, 75, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(280, 90, 491, 20))
         self.lineEdit.setObjectName("lineEdit")
-        self.graphicsView = QtWidgets.QGraphicsView(Form)
-        self.graphicsView.setGeometry(QtCore.QRect(260, 110, 256, 192))
-        self.graphicsView.setObjectName("graphicsView")
-        self.textEdit = QtWidgets.QTextEdit(Form)
-        self.textEdit.setGeometry(QtCore.QRect(70, 190, 104, 71))
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(280, 140, 401, 91))
         self.textEdit.setObjectName("textEdit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(280, 120, 54, 12))
+        self.label.setObjectName("label")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(50, 250, 731, 331))
+        self.graphicsView.setObjectName("graphicsView")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(50, 230, 101, 16))
+        self.label_2.setObjectName("label_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Form):
+
+
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "PushButton"))
-        self.pushButton_2.setText(_translate("Form", "PushButton"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "启动机器人"))
+        self.pushButton_2.setText(_translate("MainWindow", "发送消息"))
+        self.pushButton_3.setText(_translate("MainWindow", "PushButton"))
+        self.label.setText(_translate("MainWindow", "接收的消息"))
+        self.label_2.setText(_translate("MainWindow", "接收的图片"))
 
-    def open_bot( self ):
-        # self.bot = Bot(console_qr=True, cache_path=True)
-        self.bot = Bot( cache_path=True )
-    def msg( self ):
-        my_friend = self.bot.friends().search('春暖花开')[0]
+    def open_bot(self):
+        # self.bot = Bot(console_qr=True, cache_path=
+
+        self.bot = Bot(cache_path=True)
+
+    def msg(self):
+        my_friend = self.bot.friends().search('凯哥')[0]
 
         text = self.lineEdit.text()
-        my_friend.send( text )
+        my_friend.send(text)
+
+    # @Bot.register()
+    # def print_messages( self, msg ):
+    #     print(msg)
+
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
-    sys.exit(app.exec_())
 
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow(MainWindow)
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
+
+
+    sys.exit(app.exec_())
--- w_Chart.py
+++ w_Chart.py
@@ -1,10 +1,28 @@
-# 导入模块
 from wxpy import *
-# 初始化机器人，扫码登陆
-bot = Bot(console_qr=True, cache_path=True)
-# 给机器人自己发送消息
-bot.self.send('Hello World!')
-# 给文件传输助手发送消息
-bot.file_helper.send('Hello World!')
+
+bot = Bot(cache_path=True)
+# 获取好友
+my_friend = bot.friends().search('凯哥')[0]
+
+# 搜索信息
+messages = bot.messages.search(keywords='凯哥', sender=bot.self)
+
+for message in messages:
+    print(message)
+
+# 发送文本
+my_friend.send('Hello, WeChat!')
+# 发送图片
 
 
+
+# 消息接收监听器
+@bot.register()
+def print_others(msg):
+    # 输出监听到的消息
+    print(msg)
+    # 回复消息
+    msg.reply("hello world")
+
+
+embed()
